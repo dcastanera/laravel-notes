f230c07c365a65013eabbf856a87dfb0c88d0295
Dan Castanera <dcastanera@gmail.com>
2018-02-28 03:46:42 -0500

    update

--- src/NoteServiceProvider.php
+++ src/NoteServiceProvider.php
@@ -18,9 +18,12 @@ class NoteServiceProvider extends ServiceProvider
          * This copies the migrations for the roles and permissions tables to the
          * default database/migrations directory.
          */
-        $this->publishes([
-            __DIR__.'/migrations' => base_path('database/migrations'),
-        ]);
+
+        $this->loadMigrationsFrom(__DIR__.'/migrations');
+
+        // $this->publishes([
+        //     __DIR__.'/migrations' => base_path('database/migrations'),
+        // ]);
     }
 
     /**
